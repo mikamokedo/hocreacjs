8afbdeb89830446b2d192166e68e65c522f0473c
lequy <mikamokedo@gmail.com>
2019-04-14 12:03:52 +0700

    uplenhost

---
+++ precache-manifest.46838f9eb0d0c3e167f1c927b367c9bf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "d2a7b294b55109a2f94a",
+    "url": "/static/js/main.d3a21bba.chunk.js"
+  },
+  {
+    "revision": "4ebc58f2b6c1b5843bb1",
+    "url": "/static/js/2.09ca087d.chunk.js"
+  },
+  {
+    "revision": "d2a7b294b55109a2f94a",
+    "url": "/static/css/main.58487120.chunk.css"
+  },
+  {
+    "revision": "b3a4f07d9fbaca4b636d12f7d8cd5151",
+    "url": "/index.html"
+  }
+];
